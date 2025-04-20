7dad74cd5274fb98a6d8de092567c4ce4c4a3798
Lubriom <lunarmorer@gmail.com>
2025-04-20 23:10:38 +0200

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+import Express from "express";
+
+const app = Express()
+const PORT = 80
+
+app.disable('x-powered-by')
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.use((req, res) => {
+    res.status(404).send('<h1>Error 404 not found</h1>')
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port http://localhost:${PORT}`)
+})
